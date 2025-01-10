db4c386d152a666588712ff22d4ce8946f4dc007
Hilary-Collado <hilarycollado1@gmail.com>
2025-01-10 18:46:53 -0400

    agregando Iconos como componentes trabajando con el diseño de los articles contenedores de los TODOs usando tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
 import { useState } from 'react'
 import './App.css'
-import luna from '../public/images/icon-moon.svg'
-
 import circulo from '../public/images/icon-cross.svg'
 import Header from './Components/Header'
+import CrossIcon from './Components/icons/CrossIcon'
 
 const App = () => {
   return (
     <div className="bg-[url(./images/bg-mobile-light.jpg)] bg-no-repeat bg-contain bg-gray-200 min-h-screen">
 
-      <Header/>
+      <Header />
 
       <main className='container mx-auto px-4 mt-8'>
-        <article className=''>
-          <button>Circulo</button>
-          <p>complete online js course</p>
-          <button>X</button>
-        </article>
+      {/* [&>article]:px-4 => le aplica un padding de 4 a todos los article que sean hijos de un div que tenga la clase article */}
+        <div className="rounded-md bg-white [&>article]:p-4">
+          <article className='flex gap-4 py-4 border-b border-b-gray-400'>
 
-        <section>
-          <span>5 items left</span>
-          <button>clear completed</button>
-        </section>
+            <button className='rounded-full border-2 w-5 h-5 inline-block flex-none'></button>
+
+            <p className='text-gray-600 grow'>complete online js course</p>
+
+            <button className='flex-none'><CrossIcon /></button>
+          </article>
+
+          <article className='flex gap-4 py-4 border-b border-b-gray-400'>
+
+            <button className='rounded-full border-2 w-5 h-5 inline-block flex-none'></button>
+
+            <p className='text-gray-600 grow'>complete online js course</p>
+
+            <button className='flex-none'><CrossIcon /></button>
+
+          </article>
+
+          <section className='py-4 px-4 flex justify-between'>
+            <span className='text-gray-400'>5 items left</span>
+            <button className='text-gray-400'>clear completed</button>
+          </section>
+        </div>
 
         <section className='container mx-auto px-4'>
           <button>All</button>
