3ce8602967650698bf0e162136b0a4de3812a63f
Hilary-Collado <hilarycollado1@gmail.com>
2025-02-09 17:50:00 -0400

    Aplicando hover a filter

--- src/Components/TodoFilter.jsx
+++ src/Components/TodoFilter.jsx
@@ -1,24 +1,36 @@
-const TodoFilter = ({ changeFilter }) => {
+const TodoFilter = ({ changeFilter, filter }) => {
   return (
     <>
       <section className="container mx-auto mt-8">
         <div className="bg-white p-4 flex justify-center rounded-md gap-4">
           <button
-            className="hover:text-blue-600"
+            className={`${
+              filter === "all"
+                ? "text-blue-500 hover:text-gray-400"
+                : "text-gray-400 hover:text-blue-500"
+            }`}
             onClick={() => changeFilter("all")}
           >
             All
           </button>
 
           <button
-            className="hover:text-blue-600"
+            className={`${
+              filter === "active"
+                ? "text-blue-500 hover:text-gray-400"
+                : "text-gray-400 hover:text-blue-500"
+            }`}
             onClick={() => changeFilter("active")}
           >
             Active
           </button>
-          
+
           <button
-            className="hover:text-blue-600"
+            className={`${
+              filter === "completed"
+                ? "text-blue-500 hover:text-gray-400"
+                : "text-gray-400 hover:text-blue-500"
+            }`}
             onClick={() => changeFilter("completed")}
           >
             Completed
